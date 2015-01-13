d2b48709fcba7c332bcef9b0d0385b48258585e9
Ian McDaniel <ianmcdaniel@gmail.com>
2015-01-12 17:38:28 -0800

    gamera example

--- src/gamera.js
+++ src/gamera.js
@@ -1,20 +1,5 @@
 (function () {
-  var
-    E={}, // Entities
-    C={}, // Components
-    S=[], // Systems
-    P=[], // Plugins
-    A={}, // Attributes
-    running = false,
-    cache   = {};
-
-  function store(entity) {
-    for(var k in cache) {
-      var add = true;
-      k.split(".").forEach(function(c){if (!entity.components[c]) add=false;});
-      if(add) cache[k][entity.id] = entity;
-    }
-  }
+  var P=[]; //Plugins
 
   function values(obj) {
     var a=[],p;for(p in obj){a.push(obj[p]);}return a;
@@ -25,112 +10,138 @@
   }
 
   function extend(d){
-    for(var i=1; i<arguments.length; i++) {
-      var s = typeof(arguments[i]) == "function" ? arguments[i]() : arguments[i];
-      for (var p in s) if (hasOwnProperty.call(s, p)) d[p] = s[p];
+    for(var a,p,s,i=1; i<arguments.length; i++) {
+      a = arguments[i]; s = typeof(a) == "function" ? a() : a;
+      for (p in s) if (hasOwnProperty.call(s, p)) d[p] = s[p];
     }
     return d;
   }
 
-  function G (attrs){
-    this.entities   = E;
-    this.components = C;
-    this.systems    = S;
-    this.attributes = A = attrs;
-    this.C = cache;
-    P.forEach(function(p){p.call(this, this);}.bind(this));
-  }
+  var Object = extend((function(){}), {
+    mixin:function(o){
+      var p = this.prototype.init, m = o.init;
+      if(m) {
+        this.prototype.init = function(){p.apply(this, arguments); m.apply(this, arguments)}
+        delete o.init;
+      }
+      extend(this.prototype, o);
+    },    
+    extend: function(){
+      var 
+        p = "prototype",
+        c = function(){return this.init && this.init.apply(this, arguments)},
+        s = function(){ this.constructor = c;};
+      s[p] = this[p]; c[p] = new s;
+      extend(c[p], extend.apply(this, arguments));
+      for(k in Object) c[k] = Object[k]
+      return c;
+    }
+  })
 
-  G.prototype = {
+  var G = Object.extend({
 
-    get: function(k) {return A[k];},
-    set: function(k,v){A[k]=v;this.trigger('change:'+k, v)},
+    init:function(attrs){
+      this.entities   = {};
+      this.components = {};
+      this.systems    = [];
+      this.running    = false;
+      
+      this._attrs     = attrs || {};
+      this._cache     = {}
+      P.forEach(function(p){p.call(this, this);}.bind(this));
+    },
+
+    get: function(k) {return this._attrs[k];},
+    set: function(k,v){this._attrs[k]=v; this.trigger('change:'+k, v)},
 
     // create entity
     entity: function(id, components){
+      var e = this.entities;
       if(typeof id == "object") {components=id; id=uid();}
-      E[id] = new G.Entity(id, components);
-      this.trigger("entity:added", E[id]);
-      return E[id];
+      e[id] = new G.Entity(this, id, components);
+      this._store(e[id]);
+      this.trigger("entity:added", e[id]);
+      return e[id];
     },
 
     removeEntity: function(id) {
-      delete E[id];
-      for(var k in cache) cache[k][id] && delete cache[k][id];
+      delete this.entities[id];
+      for(var k in this._cache) this._cache[k][id] && delete this._cache[k][id];
       this.trigger("entity:removed", id);
     },
 
     // create component
     component: function(id, methods) {
       if(typeof id == "object") {methods = id; id = methods.id || uid();}
-      C[id] = G.Component.extend({id:id,game:this}, methods);
-      this.trigger("component:added", C[id]);
+      var c = this.components[id] = G.Component.extend({id:id,game:this}, methods);
+      this.trigger("component:added", c);
     },
 
     // create system
     system: function(id, methods) {
       if(typeof id == "object") {methods = id; id = methods.id || uid();}
       var s = new (G.System.extend({id:id, game:this}, methods))();
-      S.push(s);
+      this.systems.push(s);
       this.trigger("system:added", s);
     },
 
     removeSystem: function(id){
-      var indx;
-      S.forEach(function(s,i){if(s.id == id) indx = i;});
-      if(indx) S.splice(indx,1);
+      var indx, s = this.systems;
+      s.forEach(function(s,i){if(s.id == id) indx = i;});
+      if(indx) s.splice(indx,1);
       this.trigger("system:removed", id);
     },
 
     getEntity: function(id){
-      return E[id];
+      return this.entities[id];
     },
 
     // find entities by component
     getEntities: function(){
-      var cid = Array.prototype.slice.call(arguments, 0).sort().join(".");
-      if(cache[cid]) return values(cache[cid]);
-      cache[cid] = {};
-      for(var e in E) store(E[e]);
-      return values(cache[cid]);
+      var cid = Array.prototype.slice.call(arguments, 0).sort().join("."), c=this._cache;
+      if(c[cid]) return values(c[cid]);
+      c[cid] = {};
+      for(var e in this.entities) this._store(this.entities[e]);
+      return values(c[cid]);
     },
 
     start: function(c){
-      if(!running) {
-        S.sort(function(a,b){return a.order || 0 > b.order || 0;});
-        running = true;
+      var self = this;
+      if(!this.running) {
+        this.systems.sort(function(a,b){return a.order || 0 > b.order || 0;});
+        this.running = true;
         this.trigger("start", this);
         (function gl(){
-          S.forEach(function(s){
-            !window.paused && s.update  && s.update.call(s);
+          self.systems.forEach(function(s){
+            s.update  && s.update.call(s);
             s.draw    && s.draw.call(s);
           });
-          if(running) window.requestAnimationFrame(gl, c);
+          if(self.running) window.requestAnimationFrame(gl, c);
         })()
       }
     },
 
     stop: function(){
-      running = false;
+      this.running = false;
       this.trigger("stop", this);
-    }
-  };
-  
-  G.Object = {
-    extend: function(){
-      var f = function(){
-        return this.init && this.init.apply(this, arguments);
+    },
+
+    _store: function(entity) {
+      for(var k in this._cache) {
+        var add = true;
+        k.split(".").forEach(function(c){if (!entity.components[c]) add=false;});
+        if(add) this._cache[k][entity.id] = entity;
       }
-      extend(f.prototype, this.prototype, extend.apply(this, arguments));
-      extend(f, G.Object);
-      return f;
     }
-  };
+  })
+
+  G.Object = Object;
 
   G.Entity  = G.Object.extend({
-    init: function(id, components){
+    init: function(game, id, components){
       this.components = {};
       this.id = id;
+      this.game = game;
       for(var c in components) this.add(c, components[c]);
     },
     get:function(c){
@@ -141,24 +152,24 @@
       for(var k in cache) if(k.match(c)) cache[k] && delete cache[k][this.id];
     },
     add: function(c, a) {
+      var gc = this.game.components;
       this.components[c] = (function(c, args, fn) {
         args.constructor == Array || (args = [args]);
-        if(C[c]) {
-          fn=function(){return C[c].apply(this, args);};
-          fn.prototype = C[c].prototype;
+        if(gc[c]) {
+          fn=function(){return gc[c].apply(this, args);};
+          fn.prototype = gc[c].prototype;
           return new fn();
         }
       }.bind(this))(c, a);
-      store(this);
     }
   });
-
+  
   G.System = G.Object.extend({
     getEntity: function(){
-      return this.game.getEntity.apply(this, arguments);
+      return this.game.getEntity.apply(this.game, arguments);
     },
     getEntities: function(){
-      return this.game.getEntities.apply(this, arguments);
+      return this.game.getEntities.apply(this.game, arguments);
     }
   });
 
@@ -166,36 +177,29 @@
     init: function(p){for(var k in p || {}) this[k] = p[k];}
   });
 
-  G.extend = extend;
-
   G.plugin = function(fn){P.push(fn);};
 
-  G.Events = function(){
-    var events = {};
-    return {
-      ge:function(){
-        return events;
-      },
-      on:function(evt, fn){
-        events[evt] = events[evt] || [];
-        events[evt].push(fn);
+  G.Events = {
+    on:function(evt, fn){
+      this.events || (this.events = {})
+      this.events[evt] = this.events[evt] || [];
+      this.events[evt].push(fn);
+    },
+    off:function(evt, fn){
+      if(!this.events || !this.events[evt]) return;
+      (fn) ? this.events[evt].splice(this.events[evt].indexOf(fn), 1) : this.events[evt] = [];
+    },
+    trigger:function(evt){
+      var args = Array.prototype.slice.call(arguments, 1);
+      if(!this.events || !this.events[evt]) return;
+      this.events[evt].forEach(function(e){
+        e.apply(this, args);
+      }.bind(this));
+    }
+  }
 
-      },
-      off:function(evt, fn){
-        if(!events[evt]) return;
-        (fn) ? events[evt].splice(events[evt].indexOf(fn), 1) : events[evt] = [];
-      },
-      trigger:function(evt){
-        var args = Array.prototype.slice.call(arguments, 1);
-        if(!events[evt]) return;
-        events[evt].forEach(function(e){
-          e.apply(this, args);
-        }.bind(this));
-      }
-    };
-  };
-
-  extend(G.prototype, G.Events.call(G.prototype));
-  extend(G.System.prototype, G.Events.call(G.System.prototype));
+  G.mixin(G.Events);
+  G.Entity.mixin(G.Events);
+  G.System.mixin(G.Events);
   window.Gamera = G;
 }());
